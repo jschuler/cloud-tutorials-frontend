5f9e20f9fd39076737f82dce31b9c5002f5ee413
redallen <zallen@redhat.com>
2021-02-05 10:39:57 -0500

    can't require package.json

--- profiles/local-frontend.js
+++ profiles/local-frontend.js
@@ -1,6 +1,5 @@
-const { routes: packageRoutes } = require('../package.json');
-const SECTION = packageRoutes.prefix;
-const APP_ID = packageRoutes.appname;
+const SECTION = 'mosaic';
+const APP_ID = 'cloud-tutorials';
 const FRONTEND_PORT = 8002;
 const routes = {};
 
